1e301528eae3cfe2060aff5dbc05588a53017560
odan <odan3240@gmail.com>
2019-12-18 11:33:37 +0900

    add item controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ItemController } from './item/item.controller';
+import { ItemService } from './item/item.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ItemController],
+  providers: [AppService, ItemService],
 })
 export class AppModule {}
